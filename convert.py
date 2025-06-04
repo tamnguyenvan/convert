168e913cec26b10f526eb45ceb8472a480d0c408
Nguyen Van Tam <tamnvhustcc@gmail.com>
2025-06-04 17:12:40 +0700

    fix workflows 2

--- convert.py
+++ convert.py
@@ -4,7 +4,6 @@ import os
 input_folder = 'inputs'
 output_folder = 'outputs'
 
-# Tạo thư mục output nếu chưa tồn tại
 os.makedirs(output_folder, exist_ok=True)
 
 for filename in os.listdir(input_folder):
@@ -14,8 +13,10 @@ for filename in os.listdir(input_folder):
         output_path = os.path.join(output_folder, output_filename)
 
         with Image.open(input_path) as img:
-            # Thiết lập dpi khi lưu ảnh TIFF
-            img.save(output_path, format='TIFF', dpi=(300, 300))
+            # Chuyển sang CMYK
+            img_cmyk = img.convert('CMYK')
+            # Lưu ảnh TIFF với dpi 300
+            img_cmyk.save(output_path, format='TIFF', dpi=(300, 300))
 
-        print(f'Converted {filename} to {output_filename} with 300 dpi')
+        print(f'Converted {filename} to {output_filename} with 300 dpi and CMYK color space')
 
